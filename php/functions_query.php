b9aa30e53be6bd429a3c08d29b99c4d03b69809e
MacBook Air <macbookair@dn001169.etu.univ-rennes1.fr>
2026-05-20 10:06:32 +0200

    tg

--- php/functions_query.php
+++ php/functions_query.php
@@ -42,6 +42,26 @@ function getgamebyID($id, $mysqli)
     return $res[0] ?? null;
 }
 
+function checkUser($login, $password, $mysqli)
+{
+    $sql = "SELECT * FROM MEMBRE WHERE Username = '$login' AND Mdp = '$password'";
+    
+    return readDB($mysqli, $sql);
+}
+
+function checkUsernameExists($login, $mysqli)
+{
+    $sql = "SELECT Username FROM MEMBRE WHERE Username = '$login'";
+    return readDB($mysqli, $sql);
+}
+
+function insertMembre($nom, $prenom, $login, $mdp, $mail, $date_naiss, $mysqli)
+{
+    $sql = "INSERT INTO MEMBRE (Nom, Prenom, Username, Mdp, Mail, Date_naiss, Perm) 
+            VALUES ('$nom', '$prenom', '$login', '$mdp', '$mail', '$date_naiss', 'membre')";
+    
+    return writeDB($mysqli, $sql);
+}
 ?>
 
 
--- php/login.php
+++ php/login.php
@@ -1,11 +1,13 @@
 <?php
 ini_set('display_errors', 'on');
 session_start();
+
 require_once("./../includes/constantes.php");
 require_once("./../includes/config-bdd.php");
-require_once ('./functions_query.php');
-require_once ('./functions-DB.php');
+require_once('./functions_query.php');
+require_once('./functions-DB.php');
 
+// On verifie si le formulaire a bien envoye les donnees
 if (!isset($_POST['login']) || !isset($_POST['password'])) {
     header('Location: ../connection.php');
     exit();
@@ -14,28 +16,30 @@ if (!isset($_POST['login']) || !isset($_POST['password'])) {
 $login = $_POST['login'];
 $password = $_POST['password'];
 
-
+// On se connecte a la base de donnees
 $mysqli = connectionDB(); 
 
-$result = checkDresseur($login, $password, $mysqli);
+// On verifie si l'utilisateur existe avec le bon mot de passe
+$result = checkUser($login, $password, $mysqli);
 
-
+// Si le resultat est vide, c'est que le login ou le mot de passe est faux
 if (count($result) == 0) {
-    header('Location: ../connection.php');
+    header('Location: ../connection.php?erreur=1');
     exit();
 }
 
-
+// Si c'est bon, on recupere les informations de l'utilisateur
 $user = $result[0];
 
-$_SESSION['login'] = $user['nom_dresseur'];
-$_SESSION['id'] = $user['mdp_dresseur'];
+// On cree les variables de session pour garder l'utilisateur connecte
+$_SESSION['login'] = $user['Username'];
+$_SESSION['id'] = $user['ID_member'];
+$_SESSION['perm'] = $user['Perm']; // Tres utile pour verifier s'il est admin ou membre
 $_SESSION['connected'] = true;
-
 
 closeDB($mysqli);
 
-
+// On le renvoie vers la page d'accueil
 header('Location: ../index.php');
 exit();
 ?>
